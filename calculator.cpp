e9cf32d00c83fddc9ce1965e8ab53bf511b69c50
UtilityDev <45518053+UtilityDev@users.noreply.github.com>
2020-09-06 20:08:10 +0200

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  
+  cout << "x: ";
+  cin >> x;
+  
+  cout << "y: ";
+  cin >> y;
+  
+  sum = x + y;
+  cout << sum;
+}
